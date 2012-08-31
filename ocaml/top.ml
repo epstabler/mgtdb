8f9d64471bd30aca75c3cd78f222b12365e618a5
Ed Stabler <stabler@ucla.edu>
2012-08-31 11:30:37 -0700

    cleanup

--- ocaml/top.ml
+++ ocaml/top.ml
@@ -14,7 +14,7 @@ let time f x =
     let fx = f x in
     ( Printf.printf "parse found in %fs\n" (Sys.time() -. t); flush stdout ; fx; )
 
-let rec process lexArrays tree =
+let rec process g lexArrays tree =
   print_string ("(h for help): ");
   let linestring = read_line() in
   let input = split (regexp "[\ \t]+") linestring in
@@ -35,59 +35,63 @@ let rec process lexArrays tree =
 	  print_string "  pb to prettyprint bare tree\n";
 	  print_string "  pv to prettyprint value tree\n";
 	  print_string "  pm to prettyprint mcfg derivation tree (states as mcfg categories)\n"; 
-	  process lexArrays tree; )
+	  print_string "  pl to print the lexical items\n";
+	  process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "q"
     then 0
 
     else if List.length input > 0 && List.hd input = "pd"
-    then (pptree (dt2t tree); print_string "\n"; process lexArrays tree; )
+    then (pptree (dt2t tree); print_string "\n"; process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "ps"
-    then (pptree (st2t (dt2st tree)); print_string "\n";  process lexArrays tree; )
+    then (pptree (st2t (dt2st tree)); print_string "\n";  process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "px"
-    then (pptree (xb2t (dt2xb tree)); print_string "\n";  process lexArrays tree; )
+    then (pptree (xb2t (dt2xb tree)); print_string "\n";  process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "pb"
-    then (pptree (bt2t (dt2bt tree)); print_string "\n";  process lexArrays tree; )
+    then (pptree (bt2t (dt2bt tree)); print_string "\n";  process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "pv"
-    then (pptree (vt2t (dt2vt tree)); print_string "\n";  process lexArrays tree; )
+    then (pptree (vt2t (dt2vt tree)); print_string "\n";  process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "pm"
-    then (pptree (mcfg2t (dt2mcfg tree)); print_string "\n";  process lexArrays tree; )
+    then (pptree (mcfg2t (dt2mcfg tree)); print_string "\n";  process g lexArrays tree; )
 
     else if List.length input > 0 && List.hd input = "l"
-    then (tktree (T (".", lexArrays2stringTrees lexArrays)); process lexArrays tree; )
+    then (tktree (T (".", lexArrays2stringTrees lexArrays)); process g lexArrays tree; )
+    else if List.length input > 0 && List.hd input = "pl"
+    then (showGrammar g; process g lexArrays tree; )
+
     else if List.length input > 0 && List.hd input = "d"
-    then (tktree (dt2t tree); process lexArrays tree; )
+    then (tktree (dt2t tree); process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "s"
-    then (tktree (st2t (dt2st tree)); process lexArrays tree; )
+    then (tktree (st2t (dt2st tree)); process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "x"
-    then (tktree (xb2t (dt2xb tree)); process lexArrays tree; )
+    then (tktree (xb2t (dt2xb tree)); process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "b"
-    then (tktree (bt2t (dt2bt tree)); process lexArrays tree; )
+    then (tktree (bt2t (dt2bt tree)); process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "v"
-    then (tktree (vt2t (dt2vt tree)); process lexArrays tree; )
+    then (tktree (vt2t (dt2vt tree)); process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "m"
-    then (tktree (mcfg2t (dt2mcfg tree)); process lexArrays tree; )
+    then (tktree (mcfg2t (dt2mcfg tree)); process g lexArrays tree; )
     else if List.length input > 0 && List.hd input = "n"
     then 1
     else if List.length input > 0 && List.hd input = ";"
     then -1
     else 1
 
-let rec loop1 (sA,lA,tA) h0 ifs0 dx0 min0 dq0 linestring = (* parse and process result *)
+let rec loop1 g (sA,lA,tA) h0 ifs0 dx0 min0 dq0 linestring = (* parse and process result *)
   try
     let (tree,dq) = time (parseDQ (sA,lA,tA) min0) dq0 in
-    let n = process (sA,lA,tA) tree in
+    let n = process g (sA,lA,tA) tree in
       if n=0 (* done *)
       then ()
       else if n=1 (* get next input, build new dq *)
-      then loop0 (sA,lA,tA) h0 ifs0 dx0 min0
+      then loop0 g (sA,lA,tA) h0 ifs0 dx0 min0
       else (* if n=-1, look for another parse in current dq *)
-	loop1 (sA,lA,tA) h0 ifs0 dx0 min0 dq linestring
+	loop1 g (sA,lA,tA) h0 ifs0 dx0 min0 dq linestring
   with
       Not_Accepted -> 
 	( print_string ("Not accepted: "^linestring^"\n"); 
-	  loop0 (sA,lA,tA) h0 ifs0 dx0 min0;
+	  loop0 g (sA,lA,tA) h0 ifs0 dx0 min0;
 	)
-and loop0 (sA,lA,tA) h0 ifs0 dx0 min0 =  (* get input and set up mutable parts of initial dq *)
+and loop0 g (sA,lA,tA) h0 ifs0 dx0 min0 =  (* get input and set up mutable parts of initial dq *)
   let m: lexArray = Array.make (Array.length sA) [] in
   let mx: ix array = Array.make (Array.length sA) [] in
   let mifs: ifeature list array = Array.make (Array.length sA) [] in
@@ -99,12 +103,14 @@ and loop0 (sA,lA,tA) h0 ifs0 dx0 min0 =  (* get input and set up mutable parts o
     let linestring = read_line() in
     let input = split (regexp "[\ \t]+") linestring in
       if List.length input > 0 && List.hd input = "l"
-      then (tktree (T (".", lexArrays2stringTrees (sA,lA,tA))); loop0 (sA,lA,tA) h0 ifs0 dx0 min0 )
+      then (tktree (T (".", lexArrays2stringTrees (sA,lA,tA))); loop0 g (sA,lA,tA) h0 ifs0 dx0 min0 )
+      else if List.length input > 0 && List.hd input = "pl"
+      then (showGrammar g; loop0 g (sA,lA,tA) h0 ifs0 dx0 min0 )
       else if List.length input > 0 && List.hd input = "q"
       then ()
       else
 	let dq = (DQ.add (input,iq,prob,Nd []::[]) DQ.empty) in
-	  loop1 (sA,lA,tA) h0 ifs0 dx0 min0 dq linestring
+	  loop1 g (sA,lA,tA) h0 ifs0 dx0 min0 dq linestring
 
 let go = fun lex min0 ->
   let (start:string) = startCat in
@@ -115,7 +121,7 @@ let go = fun lex min0 ->
   let h = lA.(snd startInts) in
   let ifs: ifeature list = startInts::[] in
   let dx: ix = [] in
-    loop0 lAs h ifs dx min0
+    loop0 lex lAs h ifs dx min0
 
 let _ = go g0 0.00000001
 
